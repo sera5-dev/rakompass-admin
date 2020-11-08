245960db851676321e5eb6cd60d0e032ecef4c9b
rifqisambas <rifqi@sera5.id>
2020-11-09 05:43:21 +0700

    add new route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CrewController;
 use App\Http\Controllers\ProgramController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,10 +26,12 @@ Route::get('/programs/{id}', [ProgramController::class, 'show'])->name('programs
 Route::post('/programs', [ProgramController::class, 'store'])->name('programs-store');
 Route::post('/programs/{id}/crews', [ProgramController::class, 'storeCrew'])->name('programs-crew-store');
 Route::post('/programs/{id}/schedules', [ProgramController::class, 'storeSchedule'])->name('programs-schedule-store');
+Route::post('/programs/{id}/episodes', [ProgramController::class, 'storeEpisode'])->name('programs-episode-store');
 Route::delete('/programs/{id}/crews', [ProgramController::class, 'destroyCrew'])->name('programs-crew-destroy');
+Route::delete('/programs/{id}/episodes', [ProgramController::class, 'destroyCrew'])->name('programs-episode-destroy');
 Route::delete('/programs', [ProgramController::class, 'destroy'])->name('programs-destroy');
 
 Route::get('/crews', [CrewController::class, 'index'])->name('crews');
 Route::get('/crews/{id}', [CrewController::class, 'show'])->name('crews-show');
 Route::post('/crews', [CrewController::class, 'store'])->name('crews-store');
-Route::delete('/crews', [ControllerController::class, 'destroy'])->name('crews-destroy');
+Route::delete('/crews', [CrewsController::class, 'destroy'])->name('crews-destroy');
