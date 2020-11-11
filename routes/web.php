a0d7f3d4538aa92e288edf5a6904cd1a9958c1bc
rifqisambas <rifqi@sera5.id>
2020-11-11 07:30:07 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CrewController;
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PartnerController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +25,8 @@ use App\Http\Controllers\ContactController;
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'verify'])->name('verify');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/programs', [ProgramController::class, 'index'])->name('programs');
 Route::get('/programs/{id}', [ProgramController::class, 'show'])->name('programs-show');
@@ -38,6 +44,13 @@ Route::post('/crews', [CrewController::class, 'store'])->name('crews-store');
 Route::delete('/crews', [CrewsController::class, 'destroy'])->name('crews-destroy');
 
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts');
-Route::get('/events', [ContactController::class, 'index'])->name('events');
-Route::get('/partners', [ContactController::class, 'index'])->name('partners');
-Route::get('/about', [ContactController::class, 'index'])->name('about');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts-store');
+
+Route::get('/events', [EventController::class, 'index'])->name('events');
+Route::post('/events', [EventController::class, 'store'])->name('events-store');
+
+Route::get('/partners', [PartnerController::class, 'index'])->name('partners');
+Route::post('/partners', [PartnerController::class, 'store'])->name('partners-store');
+
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::post('/about', [AboutController::class, 'store'])->name('about');
