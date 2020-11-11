200c416dde261ed88a33f123d948b338e7546b4c
rifqisambas <rifqi@sera5.id>
2020-11-11 07:29:34 +0700

    update layouts views

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,5 @@
   <div class="footer-left">
     Copyright &copy; 2020 <div class="bullet"></div> Build By <a href="https://sera5.id/">sera5-dev</a>
   </div>
-  <div class="footer-right">
-
-  </div>
+  <div class="footer-right">j </div>
 </footer>
